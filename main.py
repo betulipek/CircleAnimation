b36286631be7aa976f31db034b126b3e1a0da732
Betul Ipek Cakar <ayhancakar73@gmail.com>
2024-02-23 09:22:59 +0300

    Code: %6 added for Color List

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ circle_ani.color("white")
 turtle_color_list = ["white", "yellow", "brown", "orange", "pink", "red", "green"]
 
 
-for element in range(8):
-    circle_ani.color(turtle_color_list[element])
+for element in range(15):
+    circle_ani.color(turtle_color_list[element % 6])
     circle_ani.circle(10 * element)
     circle_ani.circle(-10 * element)
     circle_ani.left(element)
