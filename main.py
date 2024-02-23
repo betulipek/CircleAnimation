56313514ca025225f120340b72b30447311b3191
Betul Ipek Cakar <ayhancakar73@gmail.com>
2024-02-23 09:00:24 +0300

    First Circle Animation successfuly created for Turtle projects

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle_screen = turtle.Screen()
+turtle_screen.bgcolor("black")
+turtle_screen.title("Circle Animation")
+
+circle_ani = turtle.Turtle()
+circle_ani.color("white")
+circle_ani.circle(100)
+
+turtle.done()
