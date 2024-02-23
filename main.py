c3de7932a4e03add343fa42740f9989597ec7da1
Betul Ipek Cakar <ayhancakar73@gmail.com>
2024-02-23 09:30:41 +0300

    Revision done for Turtle.done and added turtle.mainloop instead of it

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ turtle_screen.title("Circle Animation")
 circle_ani = turtle.Turtle()
 circle_ani.color("white")
 
-turtle_color_list = ["white", "yellow", "brown", "orange", "pink", "red", "green"]
+turtle_color_list = ["white", "yellow", "brown", "orange", "pink", "red", "green", "blue", "white"]
 
 
-for element in range(15):
+for element in range(16):
     circle_ani.color(turtle_color_list[element % 6])
     circle_ani.circle(10 * element)
     circle_ani.circle(-10 * element)
     circle_ani.left(element)
 
-turtle.done()
+turtle.mainloop()
+#turtle.done()
